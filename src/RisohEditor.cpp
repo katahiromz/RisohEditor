a8d83d52a67c5c2e21a7a00a3652c19ab9f53022
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2017-06-02 15:06:12 +0900

    AddResDlg

--- src/RisohEditor.cpp
+++ src/RisohEditor.cpp
@@ -2188,246 +2188,249 @@ void MainWnd_OnAddCursor(HWND hwnd)
     dialog.DialogBoxDx(hwnd, IDD_ADDCURSOR);
 }
 
-BOOL AddResDlg_OnInitDialog(HWND hwnd, HWND hwndFocus, LPARAM lParam)
+struct AddResDlg : DialogBase
 {
-    DragAcceptFiles(hwnd, TRUE);
-
-    // for Types
-    HWND hCmb1 = GetDlgItem(hwnd, cmb1);
-    const ConstantsDB::TableType& Table = g_ConstantsDB.GetTable(L"RESOURCE");
-    for (size_t i = 0; i < Table.size(); ++i)
+    BOOL OnInitDialog(HWND hwnd, HWND hwndFocus, LPARAM lParam)
     {
-        WCHAR sz[MAX_PATH];
-        wsprintfW(sz, L"%s (%lu)", Table[i].name.c_str(), Table[i].value);
-        ComboBox_AddString(hCmb1, sz);
+        DragAcceptFiles(hwnd, TRUE);
+
+        // for Types
+        HWND hCmb1 = GetDlgItem(hwnd, cmb1);
+        const ConstantsDB::TableType& Table = g_ConstantsDB.GetTable(L"RESOURCE");
+        for (size_t i = 0; i < Table.size(); ++i)
+        {
+            WCHAR sz[MAX_PATH];
+            wsprintfW(sz, L"%s (%lu)", Table[i].name.c_str(), Table[i].value);
+            ComboBox_AddString(hCmb1, sz);
+        }
+
+        // for Langs
+        HWND hCmb3 = GetDlgItem(hwnd, cmb3);
+        Cmb3_InsertLangItemsAndSelectLang(hCmb3, GetUserDefaultLangID());
+
+        return TRUE;
     }
 
-    // for Langs
-    HWND hCmb3 = GetDlgItem(hwnd, cmb3);
-    Cmb3_InsertLangItemsAndSelectLang(hCmb3, GetUserDefaultLangID());
-
-    return TRUE;
-}
-
-void AddResDlg_OnOK(HWND hwnd)
-{
-    ID_OR_STRING Type;
-    HWND hCmb1 = GetDlgItem(hwnd, cmb1);
-    const ConstantsDB::TableType& Table = g_ConstantsDB.GetTable(L"RESOURCE");
-    INT iType = ComboBox_GetCurSel(hCmb1);
-    if (iType != CB_ERR && iType < INT(Table.size()))
+    void OnOK(HWND hwnd)
     {
-        Type = WORD(Table[iType].value);
-    }
-    else
-    {
-        if (!Cmb1_CheckType(hCmb1, Type))
-            return;
-    }
-
-    HWND hCmb2 = GetDlgItem(hwnd, cmb2);
-    ID_OR_STRING Name;
-    if (!Res_HasNoName(Type) && !Cmb2_CheckName(hCmb2, Name))
-        return;
-
-    HWND hCmb3 = GetDlgItem(hwnd, cmb3);
-    WORD Lang;
-    if (!Cmb3_CheckLang(hCmb3, Lang))
-        return;
-
-    std::wstring File;
-    HWND hEdt1 = GetDlgItem(hwnd, edt1);
-    if (!Res_HasSample(Type) && !Edt1_CheckFile(hEdt1, File))
-        return;
-
-    BOOL Overwrite = FALSE;
-    INT iEntry = Res_Find(g_Entries, Type, Name, Lang);
-    if (iEntry != -1)
-    {
-        if (File.empty() && Res_HasSample(Type))
+        ID_OR_STRING Type;
+        HWND hCmb1 = GetDlgItem(hwnd, cmb1);
+        const ConstantsDB::TableType& Table = g_ConstantsDB.GetTable(L"RESOURCE");
+        INT iType = ComboBox_GetCurSel(hCmb1);
+        if (iType != CB_ERR && iType < INT(Table.size()))
         {
-            MessageBoxW(hwnd, LoadStringDx(IDS_ALREADYEXISTS), g_szTitle,
-                        MB_ICONERROR);
-            return;
-        }
-
-        INT id = MessageBoxW(hwnd, LoadStringDx(IDS_EXISTSOVERWRITE), g_szTitle,
-                             MB_ICONINFORMATION | MB_YESNOCANCEL);
-        switch (id)
-        {
-        case IDYES:
-            Overwrite = TRUE;
-            break;
-        case IDNO:
-        case IDCANCEL:
-            return;
-        }
-    }
-
-    BOOL bOK = TRUE;
-    if (File.empty() && Res_HasSample(Type))
-    {
-        if (Res_HasNoName(Type))
-        {
-            Res_DeleteNames(g_Entries, Type, Lang);
-        }
-
-        ByteStream stream;
-        if (Type == RT_ACCELERATOR)
-        {
-            DWORD Size;
-            const BYTE *pb = GetAccelSample(Size);
-            stream.assign(pb, Size);
-        }
-        else if (Type == RT_DIALOG)
-        {
-            DWORD Size;
-            const BYTE *pb = GetDialogSample(Size);
-            stream.assign(pb, Size);
-        }
-        else if (Type == RT_MENU)
-        {
-            DWORD Size;
-            const BYTE *pb = GetMenuSample(Size);
-            stream.assign(pb, Size);
-        }
-        else if (Type == RT_STRING)
-        {
-            DWORD Size;
-            const BYTE *pb = GetStringSample(Size);
-            stream.assign(pb, Size);
-            Name = 1;
-        }
-        else if (Type == RT_VERSION)
-        {
-            DWORD Size;
-            const BYTE *pb = GetVersionSample(Size);
-            stream.assign(pb, Size);
-        }
-        else if (Type == RT_HTML)
-        {
-            DWORD Size;
-            const BYTE *pb = GetHtmlSample(Size);
-            stream.assign(pb, Size);
-        }
-        else if (Type == RT_MANIFEST)
-        {
-            DWORD Size;
-            const BYTE *pb = GetManifestSample(Size);
-            stream.assign(pb, Size);
+            Type = WORD(Table[iType].value);
         }
         else
         {
-            bOK = FALSE;
-        }
-
-        if (bOK)
-        {
-            Res_AddEntry(g_Entries, Type, Name, Lang, stream.data(), FALSE);
-
-            TV_RefreshInfo(g_hTreeView, g_Entries, FALSE, FALSE);
-
-            ResEntry entry(Type, Name, Lang);
-            TV_SelectEntry(g_hTreeView, g_Entries, entry);
-        }
-    }
-
-    if (!bOK)
-    {
-        if (Overwrite)
-        {
-            if (!DoReplaceBin(hwnd, Type, Name, Lang, File))
-            {
-                MessageBoxW(hwnd, LoadStringDx(IDS_CANNOTREPLACE),
-                            NULL, MB_ICONERROR);
+            if (!Cmb1_CheckType(hCmb1, Type))
                 return;
-            }
         }
-        else
+
+        HWND hCmb2 = GetDlgItem(hwnd, cmb2);
+        ID_OR_STRING Name;
+        if (!Res_HasNoName(Type) && !Cmb2_CheckName(hCmb2, Name))
+            return;
+
+        HWND hCmb3 = GetDlgItem(hwnd, cmb3);
+        WORD Lang;
+        if (!Cmb3_CheckLang(hCmb3, Lang))
+            return;
+
+        std::wstring File;
+        HWND hEdt1 = GetDlgItem(hwnd, edt1);
+        if (!Res_HasSample(Type) && !Edt1_CheckFile(hEdt1, File))
+            return;
+
+        BOOL Overwrite = FALSE;
+        INT iEntry = Res_Find(g_Entries, Type, Name, Lang);
+        if (iEntry != -1)
         {
-            if (!DoAddBin(hwnd, Type, Name, Lang, File))
+            if (File.empty() && Res_HasSample(Type))
             {
-                MessageBoxW(hwnd, LoadStringDx(IDS_CANNOTADDRES), NULL,
+                MessageBoxW(hwnd, LoadStringDx(IDS_ALREADYEXISTS), g_szTitle,
                             MB_ICONERROR);
                 return;
             }
+
+            INT id = MessageBoxW(hwnd, LoadStringDx(IDS_EXISTSOVERWRITE), g_szTitle,
+                                 MB_ICONINFORMATION | MB_YESNOCANCEL);
+            switch (id)
+            {
+            case IDYES:
+                Overwrite = TRUE;
+                break;
+            case IDNO:
+            case IDCANCEL:
+                return;
+            }
+        }
+
+        BOOL bOK = TRUE;
+        if (File.empty() && Res_HasSample(Type))
+        {
+            if (Res_HasNoName(Type))
+            {
+                Res_DeleteNames(g_Entries, Type, Lang);
+            }
+
+            ByteStream stream;
+            if (Type == RT_ACCELERATOR)
+            {
+                DWORD Size;
+                const BYTE *pb = GetAccelSample(Size);
+                stream.assign(pb, Size);
+            }
+            else if (Type == RT_DIALOG)
+            {
+                DWORD Size;
+                const BYTE *pb = GetDialogSample(Size);
+                stream.assign(pb, Size);
+            }
+            else if (Type == RT_MENU)
+            {
+                DWORD Size;
+                const BYTE *pb = GetMenuSample(Size);
+                stream.assign(pb, Size);
+            }
+            else if (Type == RT_STRING)
+            {
+                DWORD Size;
+                const BYTE *pb = GetStringSample(Size);
+                stream.assign(pb, Size);
+                Name = 1;
+            }
+            else if (Type == RT_VERSION)
+            {
+                DWORD Size;
+                const BYTE *pb = GetVersionSample(Size);
+                stream.assign(pb, Size);
+            }
+            else if (Type == RT_HTML)
+            {
+                DWORD Size;
+                const BYTE *pb = GetHtmlSample(Size);
+                stream.assign(pb, Size);
+            }
+            else if (Type == RT_MANIFEST)
+            {
+                DWORD Size;
+                const BYTE *pb = GetManifestSample(Size);
+                stream.assign(pb, Size);
+            }
+            else
+            {
+                bOK = FALSE;
+            }
+
+            if (bOK)
+            {
+                Res_AddEntry(g_Entries, Type, Name, Lang, stream.data(), FALSE);
+
+                TV_RefreshInfo(g_hTreeView, g_Entries, FALSE, FALSE);
+
+                ResEntry entry(Type, Name, Lang);
+                TV_SelectEntry(g_hTreeView, g_Entries, entry);
+            }
+        }
+
+        if (!bOK)
+        {
+            if (Overwrite)
+            {
+                if (!DoReplaceBin(hwnd, Type, Name, Lang, File))
+                {
+                    MessageBoxW(hwnd, LoadStringDx(IDS_CANNOTREPLACE),
+                                NULL, MB_ICONERROR);
+                    return;
+                }
+            }
+            else
+            {
+                if (!DoAddBin(hwnd, Type, Name, Lang, File))
+                {
+                    MessageBoxW(hwnd, LoadStringDx(IDS_CANNOTADDRES), NULL,
+                                MB_ICONERROR);
+                    return;
+                }
+            }
+        }
+
+        EndDialog(hwnd, IDOK);
+    }
+
+    void OnPsh1(HWND hwnd)
+    {
+        WCHAR File[MAX_PATH];
+        GetDlgItemText(hwnd, edt1, File, _countof(File));
+
+        std::wstring strFile = File;
+        str_trim(strFile);
+        lstrcpynW(File, strFile.c_str(), _countof(File));
+
+        OPENFILENAMEW ofn;
+        ZeroMemory(&ofn, sizeof(ofn));
+        ofn.lStructSize = OPENFILENAME_SIZE_VERSION_400W;
+        ofn.hwndOwner = hwnd;
+        ofn.lpstrFilter = MakeFilterDx(LoadStringDx(IDS_ALLFILES));
+        ofn.lpstrFile = File;
+        ofn.nMaxFile = _countof(File);
+        ofn.lpstrTitle = LoadStringDx2(IDS_ADDRES);
+        ofn.Flags = OFN_ENABLESIZING | OFN_EXPLORER | OFN_FILEMUSTEXIST |
+            OFN_HIDEREADONLY | OFN_PATHMUSTEXIST;
+        ofn.lpstrDefExt = L"bin";
+        if (GetOpenFileNameW(&ofn))
+        {
+            SetDlgItemTextW(hwnd, edt1, File);
         }
     }
 
-    EndDialog(hwnd, IDOK);
-}
-
-void AddResDlg_OnPsh1(HWND hwnd)
-{
-    WCHAR File[MAX_PATH];
-    GetDlgItemText(hwnd, edt1, File, _countof(File));
-
-    std::wstring strFile = File;
-    str_trim(strFile);
-    lstrcpynW(File, strFile.c_str(), _countof(File));
-
-    OPENFILENAMEW ofn;
-    ZeroMemory(&ofn, sizeof(ofn));
-    ofn.lStructSize = OPENFILENAME_SIZE_VERSION_400W;
-    ofn.hwndOwner = hwnd;
-    ofn.lpstrFilter = MakeFilterDx(LoadStringDx(IDS_ALLFILES));
-    ofn.lpstrFile = File;
-    ofn.nMaxFile = _countof(File);
-    ofn.lpstrTitle = LoadStringDx2(IDS_ADDRES);
-    ofn.Flags = OFN_ENABLESIZING | OFN_EXPLORER | OFN_FILEMUSTEXIST |
-        OFN_HIDEREADONLY | OFN_PATHMUSTEXIST;
-    ofn.lpstrDefExt = L"bin";
-    if (GetOpenFileNameW(&ofn))
+    void OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify)
     {
+        switch (id)
+        {
+        case IDOK:
+            OnOK(hwnd);
+            break;
+        case IDCANCEL:
+            EndDialog(hwnd, IDCANCEL);
+            break;
+        case psh1:
+            OnPsh1(hwnd);
+            break;
+        case cmb1:
+            if (codeNotify == CBN_SELCHANGE)
+            {
+                
+            }
+            break;
+        }
+    }
+
+    void OnDropFiles(HWND hwnd, HDROP hdrop)
+    {
+        WCHAR File[MAX_PATH];
+        DragQueryFileW(hdrop, 0, File, _countof(File));
         SetDlgItemTextW(hwnd, edt1, File);
     }
-}
 
-void AddResDlg_OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify)
-{
-    switch (id)
+    virtual INT_PTR CALLBACK
+    DialogProcDx(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
     {
-    case IDOK:
-        AddResDlg_OnOK(hwnd);
-        break;
-    case IDCANCEL:
-        EndDialog(hwnd, IDCANCEL);
-        break;
-    case psh1:
-        AddResDlg_OnPsh1(hwnd);
-        break;
-    case cmb1:
-        if (codeNotify == CBN_SELCHANGE)
+        switch (uMsg)
         {
-            
+            HANDLE_MSG(hwnd, WM_INITDIALOG, OnInitDialog);
+            HANDLE_MSG(hwnd, WM_DROPFILES, OnDropFiles);
+            HANDLE_MSG(hwnd, WM_COMMAND, OnCommand);
         }
-        break;
+        return DefaultProcDx();
     }
-}
-
-void AddResDlg_OnDropFiles(HWND hwnd, HDROP hdrop)
-{
-    WCHAR File[MAX_PATH];
-    DragQueryFileW(hdrop, 0, File, _countof(File));
-    SetDlgItemTextW(hwnd, edt1, File);
-}
-
-INT_PTR CALLBACK
-AddResDlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-{
-    switch (uMsg)
-    {
-        HANDLE_MSG(hwnd, WM_INITDIALOG, AddResDlg_OnInitDialog);
-        HANDLE_MSG(hwnd, WM_DROPFILES, AddResDlg_OnDropFiles);
-        HANDLE_MSG(hwnd, WM_COMMAND, AddResDlg_OnCommand);
-    }
-    return 0;
-}
+};
 
 void MainWnd_OnAddRes(HWND hwnd)
 {
-    DialogBoxW(g_hInstance, MAKEINTRESOURCEW(IDD_ADDRES), hwnd,
-               AddResDlgProc);
+    AddResDlg dialog;
+    dialog.DialogBoxDx(hwnd, IDD_ADDRES);
 }
 
 void MainWnd_OnAbout(HWND hwnd)
