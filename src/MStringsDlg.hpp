8534fb413dc3852effc88674a1eff489f286f719
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2017-06-04 20:03:13 +0900

    refactor #28

--- src/MStringsDlg.hpp
+++ src/MStringsDlg.hpp
@@ -5,8 +5,93 @@
 #define MZC4_MSTRINGSDLG_HPP_
 
 #include "RisohEditor.hpp"
-#include "MAddStrDlg.hpp"
-#include "MModifyStrDlg.hpp"
+#include "StringRes.hpp"
+
+struct MAddStrDlg;
+struct MModifyStrDlg;
+struct MStringsDlg;
+
+//////////////////////////////////////////////////////////////////////////////
+
+struct MAddStrDlg : MDialogBase
+{
+    STRING_ENTRY& m_entry;
+
+    MAddStrDlg(STRING_ENTRY& entry) : MDialogBase(IDD_ADDSTR), m_entry(entry)
+    {
+    }
+
+    BOOL OnInitDialog(HWND hwnd, HWND hwndFocus, LPARAM lParam)
+    {
+        return TRUE;
+    }
+
+    void OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify)
+    {
+        switch (id)
+        {
+        case IDOK:
+            StrDlg_GetEntry(hwnd, m_entry);
+            EndDialog(hwnd, IDOK);
+            break;
+        case IDCANCEL:
+            EndDialog(hwnd, IDCANCEL);
+            break;
+        }
+    }
+
+    virtual INT_PTR CALLBACK
+    DialogProcDx(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+    {
+        switch (uMsg)
+        {
+            HANDLE_MSG(hwnd, WM_INITDIALOG, OnInitDialog);
+            HANDLE_MSG(hwnd, WM_COMMAND, OnCommand);
+        }
+        return 0;
+    }
+};
+
+//////////////////////////////////////////////////////////////////////////////
+
+struct MModifyStrDlg : MDialogBase
+{
+    STRING_ENTRY& m_entry;
+    MModifyStrDlg(STRING_ENTRY& entry) : MDialogBase(IDD_MODIFYSTR), m_entry(entry)
+    {
+    }
+
+    BOOL OnInitDialog(HWND hwnd, HWND hwndFocus, LPARAM lParam)
+    {
+        StrDlg_SetEntry(hwnd, m_entry);
+        return TRUE;
+    }
+
+    void OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify)
+    {
+        switch (id)
+        {
+        case IDOK:
+            StrDlg_GetEntry(hwnd, m_entry);
+            EndDialog(hwnd, IDOK);
+            break;
+        case IDCANCEL:
+            EndDialog(hwnd, IDCANCEL);
+            break;
+        }
+    }
+
+    virtual INT_PTR CALLBACK
+    DialogProcDx(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+    {
+        switch (uMsg)
+        {
+            HANDLE_MSG(hwnd, WM_INITDIALOG, OnInitDialog);
+            HANDLE_MSG(hwnd, WM_COMMAND, OnCommand);
+        }
+        return DefaultProcDx();
+    }
+};
 
 //////////////////////////////////////////////////////////////////////////////
 
