2213b0e7a1fb4875d011fe9b26c117ce30387f32
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2017-06-04 11:26:14 +0900

    converting to C++ style #3

--- src/RisohEditor.cpp
+++ src/RisohEditor.cpp
@@ -4708,104 +4708,106 @@ BOOL EditMenuDlg_OnInitDialog(HWND hwnd, HWND hwndFocus, LPARAM lParam)
     return TRUE;
 }
 
-BOOL AddMItemDlg_OnInitDialog(HWND hwnd, HWND hwndFocus, LPARAM lParam)
+struct AddMItemDlg : DialogBase
 {
-    SetWindowLongPtr(hwnd, GWLP_USERDATA, lParam);
-    MENU_ENTRY& m_entry = *(MENU_ENTRY *)lParam;
+    MENU_ENTRY& m_entry;
 
-    SetDlgItemInt(hwnd, cmb2, 0, TRUE);
-    SetDlgItemInt(hwnd, edt1, 0, TRUE);
-
-    return TRUE;
-}
-
-void AddMItemDlg_OnOK(HWND hwnd)
-{
-    LPARAM lParam = GetWindowLongPtr(hwnd, GWLP_USERDATA);
-    MENU_ENTRY& m_entry = *(MENU_ENTRY *)lParam;
-
-    GetDlgItemTextW(hwnd, cmb1, m_entry.Caption, _countof(m_entry.Caption));
-    str_trim(m_entry.Caption);
-    if (m_entry.Caption[0] == L'"')
+    AddMItemDlg(MENU_ENTRY& entry) : m_entry(entry)
     {
-        str_unquote(m_entry.Caption);
     }
 
-    GetDlgItemTextW(hwnd, cmb2, m_entry.CommandID, _countof(m_entry.CommandID));
-    str_trim(m_entry.CommandID);
-
-    DWORD dwType = 0, dwState = 0;
-    if (IsDlgButtonChecked(hwnd, chx1) == BST_CHECKED)
-        dwState |= MFS_GRAYED;
-    if (IsDlgButtonChecked(hwnd, chx3) == BST_CHECKED)
-        dwType |= MFT_BITMAP;
-    if (IsDlgButtonChecked(hwnd, chx4) == BST_CHECKED)
-        dwType |= MFT_OWNERDRAW;
-    if (IsDlgButtonChecked(hwnd, chx5) == BST_CHECKED)
-        dwState |= MFS_CHECKED;
-    if (IsDlgButtonChecked(hwnd, chx6) == BST_CHECKED)
-        dwType |= MFT_SEPARATOR;
-    if (IsDlgButtonChecked(hwnd, chx7) == BST_CHECKED)
-        dwType |= MFT_MENUBARBREAK;
-    if (IsDlgButtonChecked(hwnd, chx8) == BST_CHECKED)
-        dwType |= MFT_MENUBREAK;
-    if (IsDlgButtonChecked(hwnd, chx9) == BST_CHECKED)
-        dwState |= MFS_DEFAULT;
-    if (IsDlgButtonChecked(hwnd, chx10) == BST_CHECKED)
-        dwState |= MFS_HILITE;
-    if (IsDlgButtonChecked(hwnd, chx11) == BST_CHECKED)
-        dwType |= MFT_RADIOCHECK;
-    if (IsDlgButtonChecked(hwnd, chx12) == BST_CHECKED)
-        dwType |= MFT_RIGHTORDER;
-    if (IsDlgButtonChecked(hwnd, chx13) == BST_CHECKED)
-        dwType |= MFT_RIGHTJUSTIFY;
-
-    std::wstring str = GetMenuTypeAndState(dwType, dwState);
-    if (m_entry.Caption[0] == 0 ||
-        lstrcmpiW(m_entry.Caption, LoadStringDx(IDS_SEPARATOR)) == 0)
+    BOOL OnInitDialog(HWND hwnd, HWND hwndFocus, LPARAM lParam)
     {
-        m_entry.Caption[0] = 0;
-        dwType |= MFT_SEPARATOR;
-        str = GetMenuTypeAndState(dwType, dwState);
+        SetDlgItemInt(hwnd, cmb2, 0, TRUE);
+        SetDlgItemInt(hwnd, edt1, 0, TRUE);
+        return TRUE;
     }
-    lstrcpynW(m_entry.Flags, str.c_str(), _countof(m_entry.Flags));
 
-    ::GetDlgItemTextW(hwnd, edt1, m_entry.HelpID, _countof(m_entry.HelpID));
-
-    EndDialog(hwnd, IDOK);
-}
-
-void AddMItemDlg_OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify)
-{
-    switch (id)
+    void OnOK(HWND hwnd)
     {
-    case IDOK:
-        AddMItemDlg_OnOK(hwnd);
-        break;
-    case IDCANCEL:
-        EndDialog(hwnd, IDCANCEL);
-        break;
-    case chx6:
-        if (IsDlgButtonChecked(hwnd, chx6) == BST_CHECKED)
+        GetDlgItemTextW(hwnd, cmb1, m_entry.Caption, _countof(m_entry.Caption));
+        str_trim(m_entry.Caption);
+        if (m_entry.Caption[0] == L'"')
         {
-            SetDlgItemTextW(hwnd, cmb1, NULL);
-            SetDlgItemInt(hwnd, cmb2, 0, FALSE);
-            SetDlgItemInt(hwnd, edt1, 0, FALSE);
+            str_unquote(m_entry.Caption);
         }
-        break;
-    }
-}
 
-INT_PTR CALLBACK
-AddMItemDlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-{
-    switch (uMsg)
-    {
-        HANDLE_MSG(hwnd, WM_INITDIALOG, AddMItemDlg_OnInitDialog);
-        HANDLE_MSG(hwnd, WM_COMMAND, AddMItemDlg_OnCommand);
+        GetDlgItemTextW(hwnd, cmb2, m_entry.CommandID, _countof(m_entry.CommandID));
+        str_trim(m_entry.CommandID);
+
+        DWORD dwType = 0, dwState = 0;
+        if (IsDlgButtonChecked(hwnd, chx1) == BST_CHECKED)
+            dwState |= MFS_GRAYED;
+        if (IsDlgButtonChecked(hwnd, chx3) == BST_CHECKED)
+            dwType |= MFT_BITMAP;
+        if (IsDlgButtonChecked(hwnd, chx4) == BST_CHECKED)
+            dwType |= MFT_OWNERDRAW;
+        if (IsDlgButtonChecked(hwnd, chx5) == BST_CHECKED)
+            dwState |= MFS_CHECKED;
+        if (IsDlgButtonChecked(hwnd, chx6) == BST_CHECKED)
+            dwType |= MFT_SEPARATOR;
+        if (IsDlgButtonChecked(hwnd, chx7) == BST_CHECKED)
+            dwType |= MFT_MENUBARBREAK;
+        if (IsDlgButtonChecked(hwnd, chx8) == BST_CHECKED)
+            dwType |= MFT_MENUBREAK;
+        if (IsDlgButtonChecked(hwnd, chx9) == BST_CHECKED)
+            dwState |= MFS_DEFAULT;
+        if (IsDlgButtonChecked(hwnd, chx10) == BST_CHECKED)
+            dwState |= MFS_HILITE;
+        if (IsDlgButtonChecked(hwnd, chx11) == BST_CHECKED)
+            dwType |= MFT_RADIOCHECK;
+        if (IsDlgButtonChecked(hwnd, chx12) == BST_CHECKED)
+            dwType |= MFT_RIGHTORDER;
+        if (IsDlgButtonChecked(hwnd, chx13) == BST_CHECKED)
+            dwType |= MFT_RIGHTJUSTIFY;
+
+        std::wstring str = GetMenuTypeAndState(dwType, dwState);
+        if (m_entry.Caption[0] == 0 ||
+            lstrcmpiW(m_entry.Caption, LoadStringDx(IDS_SEPARATOR)) == 0)
+        {
+            m_entry.Caption[0] = 0;
+            dwType |= MFT_SEPARATOR;
+            str = GetMenuTypeAndState(dwType, dwState);
+        }
+        lstrcpynW(m_entry.Flags, str.c_str(), _countof(m_entry.Flags));
+
+        ::GetDlgItemTextW(hwnd, edt1, m_entry.HelpID, _countof(m_entry.HelpID));
+
+        EndDialog(hwnd, IDOK);
     }
-    return 0;
-}
+
+    void OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify)
+    {
+        switch (id)
+        {
+        case IDOK:
+            OnOK(hwnd);
+            break;
+        case IDCANCEL:
+            EndDialog(hwnd, IDCANCEL);
+            break;
+        case chx6:
+            if (IsDlgButtonChecked(hwnd, chx6) == BST_CHECKED)
+            {
+                SetDlgItemTextW(hwnd, cmb1, NULL);
+                SetDlgItemInt(hwnd, cmb2, 0, FALSE);
+                SetDlgItemInt(hwnd, edt1, 0, FALSE);
+            }
+            break;
+        }
+    }
+
+    virtual INT_PTR CALLBACK
+    DialogProcDx(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+    {
+        switch (uMsg)
+        {
+            HANDLE_MSG(hwnd, WM_INITDIALOG, OnInitDialog);
+            HANDLE_MSG(hwnd, WM_COMMAND, OnCommand);
+        }
+        return DefaultProcDx();
+    }
+};
 
 void EditMenuDlg_OnAdd(HWND hwnd)
 {
@@ -4813,8 +4815,8 @@ void EditMenuDlg_OnAdd(HWND hwnd)
 
     MENU_ENTRY m_entry;
     ZeroMemory(&m_entry, sizeof(m_entry));
-    INT nID = DialogBoxParamW(g_hInstance, MAKEINTRESOURCEW(IDD_ADDMITEM),
-                              hwnd, AddMItemDlgProc, (LPARAM)&m_entry);
+    AddMItemDlg dialog(m_entry);
+    INT nID = dialog.DialogBoxDx(hwnd, IDD_ADDMITEM);
     if (IDOK != nID)
     {
         return;
