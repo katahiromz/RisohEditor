68546eb3fd67a10b50c6f0f149e092c4ec21abb5
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2017-06-11 20:16:48 +0900

    improve MDlgPropDlg

--- src/MDlgPropDlg.hpp
+++ src/MDlgPropDlg.hpp
@@ -26,35 +26,48 @@ public:
     BOOL OnInitDialog(HWND hwnd, HWND hwndFocus, LPARAM lParam)
     {
         ::SetDlgItemTextW(hwnd, cmb1, m_dialog_res.m_Title.c_str_or_empty());
+        ::SendDlgItemMessage(hwnd, cmb1, CB_LIMITTEXT, 64, 0);
 
         if (m_dialog_res.IsExtended())
+        {
             ::CheckDlgButton(hwnd, chx1, BST_CHECKED);
+        }
 
         ::SetDlgItemInt(hwnd, edt1, m_dialog_res.m_pt.x, TRUE);
         ::SetDlgItemInt(hwnd, edt2, m_dialog_res.m_pt.y, TRUE);
         ::SetDlgItemInt(hwnd, edt3, m_dialog_res.m_siz.cx, TRUE);
         ::SetDlgItemInt(hwnd, edt4, m_dialog_res.m_siz.cy, TRUE);
+        ::SendDlgItemMessage(hwnd, edt1, EM_SETLIMITTEXT, 12, 0);
+        ::SendDlgItemMessage(hwnd, edt2, EM_SETLIMITTEXT, 12, 0);
+        ::SendDlgItemMessage(hwnd, edt3, EM_SETLIMITTEXT, 12, 0);
+        ::SendDlgItemMessage(hwnd, edt4, EM_SETLIMITTEXT, 12, 0);
 
         ::SetDlgItemTextW(hwnd, cmb2, m_dialog_res.m_Class.c_str_or_empty());
-
-        ::SetDlgItemInt(hwnd, edt4, m_dialog_res.m_siz.cy, TRUE);
+        ::SendDlgItemMessage(hwnd, cmb2, CB_LIMITTEXT, 64, 0);
 
         ::SetDlgItemInt(hwnd, cmb3, m_dialog_res.m_HelpID, FALSE);
+        ::SendDlgItemMessage(hwnd, cmb2, CB_LIMITTEXT, 12, 0);
 
         ::SetDlgItemTextW(hwnd, cmb4, m_dialog_res.m_TypeFace.c_str_or_empty());
+        ::SendDlgItemMessage(hwnd, cmb4, CB_LIMITTEXT, LF_FULLFACESIZE - 1, 0);
+
         ::SetDlgItemInt(hwnd, edt5, m_dialog_res.m_PointSize, TRUE);
+        ::SendDlgItemMessage(hwnd, edt5, EM_SETLIMITTEXT, 12, 0);
 
         HWND hCmb5 = GetDlgItem(hwnd, cmb5);
         InitLangComboBox(hCmb5, m_dialog_res.m_LangID);
 
         ::SetDlgItemTextW(hwnd, cmb6, m_dialog_res.m_Menu.c_str_or_empty());
+        ::SendDlgItemMessage(hwnd, cmb6, CB_LIMITTEXT, 64, 0);
 
         WCHAR Buf[32];
 
         wsprintfW(Buf, L"%08X", m_dialog_res.m_Style);
         ::SetDlgItemTextW(hwnd, edt6, Buf);
+        ::SendDlgItemMessage(hwnd, edt6, EM_SETLIMITTEXT, 8, 0);
         wsprintfW(Buf, L"%08X", m_dialog_res.m_ExStyle);
         ::SetDlgItemTextW(hwnd, edt7, Buf);
+        ::SendDlgItemMessage(hwnd, edt7, EM_SETLIMITTEXT, 8, 0);
 
         return TRUE;
     }
