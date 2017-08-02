445e50afad01e86351799e3676a7edcccea6628f
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2017-08-02 15:21:40 +0900

    improve MAddCtrlDlg

--- src/MAddCtrlDlg.hpp
+++ src/MAddCtrlDlg.hpp
@@ -19,9 +19,12 @@ class MAddCtrlDlg : public MDialogBase
 {
 public:
     DialogRes&  m_dialog_res;
+    BOOL m_bUpdating;
+    DWORD m_dwStyle;
+    DWORD m_dwExStyle;
 
     MAddCtrlDlg(DialogRes& dialog_res)
-        : MDialogBase(IDD_ADDCTRL), m_dialog_res(dialog_res)
+        : MDialogBase(IDD_ADDCTRL), m_dialog_res(dialog_res), m_bUpdating(FALSE)
     {
     }
 
@@ -40,6 +43,9 @@ public:
                 ConstantsDB::TableType::iterator it, end = table.end();
                 for (it = table.begin(); it != end; ++it)
                 {
+                    if (it->value == 0)
+                        continue;
+
                     ListBox_AddString(hLst1, it->name.c_str());
                 }
             }
@@ -50,6 +56,9 @@ public:
             ConstantsDB::TableType::iterator it, end = table.end();
             for (it = table.begin(); it != end; ++it)
             {
+                if (it->value == 0)
+                    continue;
+
                 ListBox_AddString(hLst1, it->name.c_str());
             }
         }
@@ -68,6 +77,9 @@ public:
             ConstantsDB::TableType::iterator it, end = table.end();
             for (it = table.begin(); it != end; ++it)
             {
+                if (it->value == 0)
+                    continue;
+
                 ListBox_AddString(hLst2, it->name.c_str());
             }
         }
@@ -81,11 +93,13 @@ public:
 
         ConstantsDB::TableType table;
 
-        table = g_ConstantsDB.GetTable(TEXT("CONTROL.CLASSES"));
-        ConstantsDB::TableType::iterator it, end = table.end();
-        for (it = table.begin(); it != end; ++it)
         {
-            ComboBox_AddString(hCmb1, it->name.c_str());
+            table = g_ConstantsDB.GetTable(TEXT("CONTROL.CLASSES"));
+            ConstantsDB::TableType::iterator it, end = table.end();
+            for (it = table.begin(); it != end; ++it)
+            {
+                ComboBox_AddString(hCmb1, it->name.c_str());
+            }
         }
 
         HWND hLst1 = GetDlgItem(hwnd, lst1);
@@ -94,12 +108,157 @@ public:
         HWND hLst2 = GetDlgItem(hwnd, lst2);
         InitExStyleListBox(hLst2);
 
+        TCHAR szText[64];
+        m_dwStyle = WS_VISIBLE | WS_CHILD;
+        wsprintf(szText, TEXT("%08lX"), m_dwStyle);
+        SetDlgItemText(hwnd, edt6, szText);
+
+        m_dwExStyle = 0;
+        wsprintf(szText, TEXT("%08lX"), m_dwExStyle);
+        SetDlgItemText(hwnd, edt7, szText);
+
         return TRUE;
     }
 
     void OnOK(HWND hwnd)
     {
         EndDialog(IDOK);
+    }
+
+    void OnLst1(HWND hwnd)
+    {
+        if (m_bUpdating)
+            return;
+
+        extern ConstantsDB g_ConstantsDB;
+
+        TCHAR szText[64];
+        MString strClass = GetDlgItemText(hwnd, cmb1);
+        mstr_trim(strClass);
+
+        INT sels[64];
+        HWND hLst1 = GetDlgItem(hwnd, lst1);
+        INT nCount = ListBox_GetSelItems(hLst1, _countof(sels), sels);
+        DWORD dwStyle = 0;
+        for (INT i = 0; i < nCount; ++i)
+        {
+            ListBox_GetText(hLst1, sels[i], szText);
+
+            DWORD dwValue = g_ConstantsDB.GetValue(strClass, szText);
+            if (dwValue == 0)
+                dwValue = g_ConstantsDB.GetValue(TEXT("STYLE"), szText);
+            dwStyle |= dwValue;
+        }
+
+        m_dwStyle = dwStyle;
+        wsprintf(szText, TEXT("%08lX"), m_dwStyle);
+        m_bUpdating = TRUE;
+        SetDlgItemText(hwnd, edt6, szText);
+        m_bUpdating = FALSE;
+    }
+
+    void OnLst2(HWND hwnd)
+    {
+        if (m_bUpdating)
+            return;
+
+        extern ConstantsDB g_ConstantsDB;
+
+        TCHAR szText[64];
+        INT sels[64];
+        HWND hLst2 = GetDlgItem(hwnd, lst2);
+        INT nCount = ListBox_GetSelItems(hLst2, _countof(sels), sels);
+        DWORD dwExStyle = 0;
+        for (INT i = 0; i < nCount; ++i)
+        {
+            ListBox_GetText(hLst2, sels[i], szText);
+
+            DWORD dwValue = g_ConstantsDB.GetValue(TEXT("EXSTYLE"), szText);
+            dwExStyle |= dwValue;
+        }
+
+        m_dwExStyle = dwExStyle;
+        wsprintf(szText, TEXT("%08lX"), m_dwExStyle);
+        m_bUpdating = TRUE;
+        SetDlgItemText(hwnd, edt7, szText);
+        m_bUpdating = FALSE;
+    }
+
+    void OnEdt6(HWND hwnd)
+    {
+        if (m_bUpdating)
+            return;
+
+        extern ConstantsDB g_ConstantsDB;
+
+        MString strClass = GetDlgItemText(hwnd, cmb1);
+        mstr_trim(strClass);
+
+        MString text = GetDlgItemText(hwnd, edt6);
+        mstr_trim(text);
+        DWORD dwStyle = _tcstoul(text.c_str(), NULL, 16);
+
+        TCHAR szText[64];
+        INT sels[64];
+        HWND hLst1 = GetDlgItem(hwnd, lst1);
+        INT nCount = ListBox_GetSelItems(hLst1, _countof(sels), sels);
+        for (INT i = 0; i < nCount; ++i)
+        {
+            ListBox_GetText(hLst1, sels[i], szText);
+
+            DWORD dwValue = g_ConstantsDB.GetValue(strClass, szText);
+            if (dwValue == 0)
+                dwValue = g_ConstantsDB.GetValue(TEXT("STYLE"), szText);
+            if (dwValue == 0)
+                continue;
+
+            m_bUpdating = TRUE;
+            if ((dwStyle & dwValue) == dwValue)
+            {
+                ListBox_SetSel(hLst1, TRUE, i);
+            }
+            else
+            {
+                ListBox_SetSel(hLst1, FALSE, i);
+            }
+            m_bUpdating = FALSE;
+        }
+    }
+
+    void OnEdt7(HWND hwnd)
+    {
+        if (m_bUpdating)
+            return;
+
+        extern ConstantsDB g_ConstantsDB;
+
+        MString text = GetDlgItemText(hwnd, edt6);
+        mstr_trim(text);
+        DWORD dwExStyle = _tcstoul(text.c_str(), NULL, 16);
+
+        TCHAR szText[64];
+        INT sels[64];
+        HWND hLst2 = GetDlgItem(hwnd, lst2);
+        INT nCount = ListBox_GetSelItems(hLst2, _countof(sels), sels);
+        for (INT i = 0; i < nCount; ++i)
+        {
+            ListBox_GetText(hLst2, sels[i], szText);
+
+            DWORD dwValue = g_ConstantsDB.GetValue(TEXT("EXSTYLE"), szText);
+            if (dwValue == 0)
+                continue;
+
+            m_bUpdating = TRUE;
+            if ((dwExStyle & dwValue) == dwValue)
+            {
+                ListBox_SetSel(hLst2, TRUE, i);
+            }
+            else
+            {
+                ListBox_SetSel(hLst2, FALSE, i);
+            }
+            m_bUpdating = FALSE;
+        }
     }
 
     void OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify)
@@ -129,6 +288,30 @@ public:
                 MString text = GetDlgItemText(hwnd, cmb1);
                 mstr_trim(text);
                 InitStyleListBox(hLst1, text.c_str());
+            }
+            break;
+        case lst1:
+            if (codeNotify == LBN_SELCHANGE)
+            {
+                OnLst1(hwnd);
+            }
+            break;
+        case lst2:
+            if (codeNotify == LBN_SELCHANGE)
+            {
+                OnLst2(hwnd);
+            }
+            break;
+        case edt6:
+            if (codeNotify == EN_CHANGE)
+            {
+                OnEdt6(hwnd);
+            }
+            break;
+        case edt7:
+            if (codeNotify == EN_CHANGE)
+            {
+                OnEdt7(hwnd);
             }
             break;
         }
