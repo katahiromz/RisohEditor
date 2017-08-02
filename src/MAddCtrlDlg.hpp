99666e73403c74041dcaa0f0855b84389c16fe7d
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2017-08-02 14:53:52 +0900

    MAddCtrlDlg init style listbox

--- src/MAddCtrlDlg.hpp
+++ src/MAddCtrlDlg.hpp
@@ -6,6 +6,7 @@
 
 #include "RisohEditor.hpp"
 #include "DialogRes.hpp"
+#include "MString.hpp"
 #include "resource.h"
 
 void InitLangComboBox(HWND hCmb3, LANGID langid);
@@ -24,19 +25,74 @@ public:
     {
     }
 
+    void InitStyleListBox(HWND hLst1, LPCTSTR pszClass)
+    {
+        extern ConstantsDB g_ConstantsDB;
+
+        ListBox_ResetContent(hLst1);
+
+        ConstantsDB::TableType table;
+        if (pszClass[0])
+        {
+            table = g_ConstantsDB.GetTable(pszClass);
+            if (table.size())
+            {
+                ConstantsDB::TableType::iterator it, end = table.end();
+                for (it = table.begin(); it != end; ++it)
+                {
+                    ListBox_AddString(hLst1, it->name.c_str());
+                }
+            }
+        }
+        table = g_ConstantsDB.GetTable(TEXT("STYLE"));
+        if (table.size())
+        {
+            ConstantsDB::TableType::iterator it, end = table.end();
+            for (it = table.begin(); it != end; ++it)
+            {
+                ListBox_AddString(hLst1, it->name.c_str());
+            }
+        }
+    }
+
+    void InitExStyleListBox(HWND hLst2)
+    {
+        extern ConstantsDB g_ConstantsDB;
+
+        ListBox_ResetContent(hLst2);
+
+        ConstantsDB::TableType table;
+        table = g_ConstantsDB.GetTable(TEXT("EXSTYLE"));
+        if (table.size())
+        {
+            ConstantsDB::TableType::iterator it, end = table.end();
+            for (it = table.begin(); it != end; ++it)
+            {
+                ListBox_AddString(hLst2, it->name.c_str());
+            }
+        }
+    }
+
     BOOL OnInitDialog(HWND hwnd, HWND hwndFocus, LPARAM lParam)
     {
         extern ConstantsDB g_ConstantsDB;
 
         HWND hCmb1 = GetDlgItem(hwnd, cmb1);
 
-        ConstantsDB::TableType table =
-            g_ConstantsDB.GetTable(TEXT("CONTROL.CLASSES"));
+        ConstantsDB::TableType table;
+
+        table = g_ConstantsDB.GetTable(TEXT("CONTROL.CLASSES"));
         ConstantsDB::TableType::iterator it, end = table.end();
         for (it = table.begin(); it != end; ++it)
         {
             ComboBox_AddString(hCmb1, it->name.c_str());
         }
+
+        HWND hLst1 = GetDlgItem(hwnd, lst1);
+        InitStyleListBox(hLst1, TEXT(""));
+
+        HWND hLst2 = GetDlgItem(hwnd, lst2);
+        InitExStyleListBox(hLst2);
 
         return TRUE;
     }
@@ -48,6 +104,9 @@ public:
 
     void OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify)
     {
+        HWND hLst1 = GetDlgItem(hwnd, lst1);
+        HWND hCmb1 = GetDlgItem(hwnd, cmb1);
+        TCHAR szText[64];
         switch (id)
         {
         case IDOK:
@@ -55,6 +114,22 @@ public:
             break;
         case IDCANCEL:
             EndDialog(IDCANCEL);
+            break;
+        case cmb1:
+            if (codeNotify == CBN_SELCHANGE)
+            {
+                INT nIndex = ComboBox_GetCurSel(hCmb1);
+                ComboBox_GetLBText(hCmb1, nIndex, szText);
+                MString text = szText;
+                mstr_trim(text);
+                InitStyleListBox(hLst1, text.c_str());
+            }
+            else if (codeNotify == CBN_EDITCHANGE)
+            {
+                MString text = GetDlgItemText(hwnd, cmb1);
+                mstr_trim(text);
+                InitStyleListBox(hLst1, text.c_str());
+            }
             break;
         }
     }
