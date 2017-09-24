e00f0241a2ef581c137ac97ddaec384b2a93e07c
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2017-09-24 18:36:36 +0900

    fix manifest matter

--- tests/ResTest.cpp
+++ tests/ResTest.cpp
@@ -16,6 +16,15 @@ using std::FILE;
 using std::fopen;
 using std::fprintf;
 
+#ifdef _MSC_VER
+#pragma comment(linker,"/manifestdependency:\"type='win32' \
+    name='Microsoft.Windows.Common-Controls' \
+    version='6.0.0.0' \
+    processorArchitecture='*' \
+    publicKeyToken='6595b64144ccf1df' \
+    language='*'\"")
+#endif
+
 void DumpBinary(FILE *fp, LPCVOID pv, DWORD Size, const char *VarName)
 {
     const BYTE *pb = (const BYTE *)pv;
