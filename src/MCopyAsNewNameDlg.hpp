2766341fa4973923491e6524fd0f1b615e2397d0
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2018-02-03 17:28:56 +0900

    improve MCopyAsNewNameDlg

--- src/MCopyAsNewNameDlg.hpp
+++ src/MCopyAsNewNameDlg.hpp
@@ -165,7 +165,15 @@ public:
         case IDCANCEL:
             EndDialog(IDCANCEL);
             break;
+        case psh1:
+            OnPsh1(hwnd);
+            break;
         }
+    }
+
+    void OnPsh1(HWND hwnd)
+    {
+        SendMessage(GetParent(hwnd), WM_COMMAND, CMDID_IDLIST, 0);
     }
 };
 
