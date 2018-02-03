91346d902bee73b1f5bbf002119299cd10d00d37
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2018-02-04 07:42:59 +0900

    rm stdafx.hpp

--- src/RisohEditor.cpp
+++ src/RisohEditor.cpp
@@ -17,7 +17,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //////////////////////////////////////////////////////////////////////////////
 
-#include "stdafx.hpp"
+#include "RisohEditor.hpp"
 
 #pragma comment(lib, "msimg32.lib")
 
