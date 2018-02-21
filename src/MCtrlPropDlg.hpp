3845a1e812ba9a86dc1aae641f676705189d5a6d
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2018-02-21 19:09:11 +0900

    trivial

--- src/MCtrlPropDlg.hpp
+++ src/MCtrlPropDlg.hpp
@@ -290,7 +290,7 @@ public:
                 if ((style & SS_TYPEMASK) == SS_ICON ||
                     (style & SS_TYPEMASK) == SS_BITMAP)
                 {
-					using namespace std;
+                    using namespace std;
                     if (iswdigit(item.m_title.str()[0]))
                     {
                         LONG n = wcstol(item.m_title.c_str(), NULL, 0);
