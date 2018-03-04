6b5e136c9f6c3f7651cc8cb5abaf517313c80527
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2018-03-04 21:22:50 +0900

    add IDD_MACROS

--- src/resource.h
+++ src/resource.h
@@ -172,6 +172,7 @@
 #define IDD_MODIFYMSG       142
 #define IDD_MESSAGES        143
 #define IDD_FONTS           144
+#define IDD_MACROS          145
 
 #define IDS_APPNAME         1
 #define IDS_TITLEWITHFILE   2
