e5aaa229ef1ba3864c0d1803e050de63b1526191
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2018-03-14 19:50:44 +0900

    update resource.h

--- src/resource.h
+++ src/resource.h
@@ -1,25 +1,6 @@
-// resource.h --- Resource IDs for RisohEditor
-//////////////////////////////////////////////////////////////////////////////
-// RisohEditor --- Another free Win32 resource editor
-// Copyright (C) 2017-2018 Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
-// 
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-// 
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//////////////////////////////////////////////////////////////////////////////
-
-// {{NO_DEPENDENCIES}}
-#define APSTUDIO_READONLY_SYMBOLS
-
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ Compatible
+// RisohEditor_res.rc
 #define IDR_MAINFRAME           1
 
 #define IDI_MAIN                100
@@ -358,10 +339,10 @@
 
 #ifdef APSTUDIO_INVOKED
     #ifndef APSTUDIO_READONLY_SYMBOLS
-        #define _APS_NO_MFC                 130
-        #define _APS_NEXT_RESOURCE_VALUE    201
-        #define _APS_NEXT_COMMAND_VALUE     216
-        #define _APS_NEXT_CONTROL_VALUE     100
+        #define _APS_NO_MFC                 1
+        #define _APS_NEXT_RESOURCE_VALUE    1000
+        #define _APS_NEXT_COMMAND_VALUE     220
+        #define _APS_NEXT_CONTROL_VALUE     1002
         #define _APS_NEXT_SYMED_VALUE       300
     #endif
 #endif
