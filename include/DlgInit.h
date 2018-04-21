0b23a05b4e5ad5fcb5ec134dd17c10e615ed8265
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2018-04-21 22:33:40 +0900

    update DlgInit

--- include/DlgInit.h
+++ include/DlgInit.h
@@ -3,7 +3,7 @@
 //////////////////////////////////////////////////////////////////////////////
 
 #ifndef MZC4_DLGINIT_H_
-#define MZC4_DLGINIT_H_     4   /* Version 4 */
+#define MZC4_DLGINIT_H_     5   /* Version 5 */
 
 // RT_DLGINIT
 // BOOL ExecuteDlgInitDx(HWND hwnd, HMODULE module, const TCHAR *res_name);
@@ -22,75 +22,73 @@
         #define RT_DLGINIT  MAKEINTRESOURCE(240)
     #endif
 
+    // Win16 messages
+    #define WIN16_LB_ADDSTRING  0x0401
+    #define WIN16_CB_ADDSTRING  0x0403
+    #define AFX_CB_ADDSTRING    0x1234
+
     //////////////////////////////////////////////////////////////////////////////
+
+    inline const UNALIGNED WORD *
+    ExecuteDlgInitEntryDx(HWND hwnd, const UNALIGNED WORD *pw)
+    {
+        // get data
+        WORD ctrl = *pw++;
+        WORD msg = *pw++;
+        DWORD dwLen = *reinterpret_cast<const UNALIGNED DWORD *&>(pw)++;
+
+        // convert Win16 messages
+        switch (msg)
+        {
+        case AFX_CB_ADDSTRING: msg = CBEM_INSERTITEM; break;
+        case WIN16_LB_ADDSTRING: msg = LB_ADDSTRING; break;
+        case WIN16_CB_ADDSTRING: msg = CB_ADDSTRING; break;
+        }
+
+        // NOTE: We don't support OCC.
+        assert(msg == LB_ADDSTRING || msg == CB_ADDSTRING ||
+               msg == CBEM_INSERTITEM);
+
+#ifndef NDEBUG
+        assert(*(reinterpret_cast<const BYTE *>(pw) + (dwLen - 1)) == 0);
+#endif
+
+        // send the message
+        if (msg == CBEM_INSERTITEM)
+        {
+            MString text = MAnsiToText(CP_ACP, LPSTR(pw)).c_str();
+
+            COMBOBOXEXITEM item;
+            item.mask = CBEIF_TEXT;
+            item.iItem = -1;
+            item.pszText = &text[0];
+
+            if (::SendDlgItemMessageA(hwnd, ctrl, msg, 0, LPARAM(&item)) == -1)
+                return NULL;
+        }
+        else
+        {
+            if (::SendDlgItemMessageA(hwnd, ctrl, msg, 0, LPARAM(pw)) == -1)
+                return NULL;
+        }
+
+        // go to next entry
+        return reinterpret_cast<const UNALIGNED WORD *>(
+            reinterpret_cast<const UNALIGNED BYTE *>(pw) + dwLen);
+    }
 
     inline BOOL
     ExecuteDlgInitDataDx(HWND hwnd, const void *pData)
     {
-        if (!pData)
-            return FALSE;   // failure
-
-        BOOL bSuccess = TRUE;
-        const UNALIGNED WORD *pw =
-            reinterpret_cast<const UNALIGNED WORD *>(pData);
-        while (bSuccess && *pw != 0)
+        const UNALIGNED WORD *pw;
+        pw = reinterpret_cast<const UNALIGNED WORD *>(pData);
+        while (pw && *pw)
         {
-            // get data
-            WORD nIDC_ = *pw++;
-            WORD msg = *pw++;
-            DWORD dwLen = *reinterpret_cast<const UNALIGNED DWORD *&>(pw)++;
-
-#define WIN16_LB_ADDSTRING  0x0401
-#define WIN16_CB_ADDSTRING  0x0403
-#define AFX_CB_ADDSTRING    0x1234
-
-            // convert 16-bit messages
-            switch (msg)
-            {
-            case AFX_CB_ADDSTRING: msg = CBEM_INSERTITEM; break;
-            case WIN16_LB_ADDSTRING: msg = LB_ADDSTRING; break;
-            case WIN16_CB_ADDSTRING: msg = CB_ADDSTRING; break;
-            }
-
-            // NOTE: There is no OCC support.
-            assert(msg == LB_ADDSTRING || msg == CB_ADDSTRING ||
-                   msg == CBEM_INSERTITEM);
-
-#ifndef NDEBUG
-            if (msg == LB_ADDSTRING || msg == CB_ADDSTRING ||
-                msg == CBEM_INSERTITEM)
-            {
-                assert(*(reinterpret_cast<const BYTE *>(pw) + (dwLen - 1)) == 0);
-            }
-#endif
-
-            // send the message
-            if (msg == CBEM_INSERTITEM)
-            {
-                MString text = MAnsiToText(CP_ACP, LPSTR(pw)).c_str();
-
-                COMBOBOXEXITEM item;
-                item.mask = CBEIF_TEXT;
-                item.iItem = -1;
-                item.pszText = &text[0];
-
-                if (::SendDlgItemMessageA(hwnd, nIDC_, msg, 0, LPARAM(&item)) == -1)
-                    bSuccess = FALSE;
-            }
-            else
-            {
-                if (::SendDlgItemMessageA(hwnd, nIDC_, msg, 0, LPARAM(pw)) == -1)
-                    bSuccess = FALSE;
-            }
-
-            // go to next entry
-            pw = reinterpret_cast<const WORD *>(
-                reinterpret_cast<const BYTE *>(pw) + dwLen);
+            pw = ExecuteDlgInitEntryDx(hwnd, pw);
         }
 
         // NOTE: We don't send WM_INITIALUPDATE messages.
-
-        return bSuccess;
+        return pw != NULL;
     }
 
     //////////////////////////////////////////////////////////////////////////////
