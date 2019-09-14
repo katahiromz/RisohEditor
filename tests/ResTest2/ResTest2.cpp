a1e8849962029f7a3e6c791283050faf5e426306
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2019-09-14 17:58:30 +0900

    add ResTest2 test

---
+++ tests/ResTest2/ResTest2.cpp
@@ -0,0 +1,39 @@
+#include <windows.h>
+#include <windowsx.h>
+
+BOOL OnInitDialog(HWND hwnd, HWND hwndFocus, LPARAM lParam)
+{
+    return TRUE;
+}
+
+void OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify)
+{
+    switch (id)
+    {
+    case IDOK:
+    case IDCANCEL:
+        EndDialog(hwnd, id);
+        break;
+    }
+}
+
+INT_PTR CALLBACK
+DialogProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+        HANDLE_MSG(hwnd, WM_INITDIALOG, OnInitDialog);
+        HANDLE_MSG(hwnd, WM_COMMAND, OnCommand);
+    }
+    return 0;
+}
+
+INT WINAPI
+WinMain(HINSTANCE   hInstance,
+        HINSTANCE   hPrevInstance,
+        LPSTR       lpCmdLine,
+        INT         nCmdShow)
+{
+    DialogBox(hInstance, MAKEINTRESOURCE(1), NULL, DialogProc);
+    return 0;
+}
