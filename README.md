35a6369de99f08d01c6e0e7ec8c8d4eb18cb5f8b
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2020-03-03 00:59:37 +0900

    revert

--- README.md
+++ README.md
@@ -162,7 +162,6 @@ Use UTF-16 (but UTF-16 is not supported in GNU windres).
     - Made the "Output RC files as UTF-16" option non-volatile.
     - Bring "List of Resource IDs" window to top.
 - v.5.3.3 (2020.XX.YY)
-    - Adopted the xz format.
 
 ## Contact Us
 
--- pack.sh
+++ pack.sh
@@ -23,12 +23,12 @@ RE_VERSION=5.3.3
 RE_NAME="RisohEditor-$RE_VERSION"
 RE_BIN_DIR="build/$RE_NAME"
 RE_FILES="README.txt READMEJP.txt LICENSE.txt Standardize.md HYOJUNKA.txt TRANSLATORS.txt src/resource.h build/RisohEditor.exe mcdx/MESSAGETABLEDX.md"
-RE_TARGET="build/RisohEditor-$RE_VERSION.xz"
+RE_TARGET="build/RisohEditor-$RE_VERSION.zip"
 
 ################################################################################
 
-if ! which tar > /dev/null 2>&1; then
-    echo ERROR: there is no executable tar.
+if ! which zip > /dev/null 2>&1; then
+    echo ERROR: there is no executable zip.
     exit 10
 fi
 
@@ -81,7 +81,7 @@ if cp $RE_FILES "$RE_BIN_DIR"; then
                         if cp -f "src/DlgInit.h" "$RE_BIN_DIR/DlgInit"; then
                             echo Zipping...
                             cd build
-                            if tar -Jcf "$RE_NAME.xz" "$RE_NAME"; then
+                            if zip -9 -r -q "$RE_NAME.zip" "$RE_NAME"; then
                                 cd ..
                                 if [ -e "$RE_TARGET" ]; then
                                     echo Success. "$RE_TARGET" was generated.
