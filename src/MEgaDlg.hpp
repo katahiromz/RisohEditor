a6d5f201ed2894cd3afd5e35d52e14197b41dd36
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2020-03-18 21:54:59 +0900

    trivial

--- src/MEgaDlg.hpp
+++ src/MEgaDlg.hpp
@@ -47,7 +47,7 @@ static bool EGA_dialog_input(char *buf, size_t buflen)
     SetDlgItemTextA(s_hwndEga, cmb1, NULL);
     s_bEnter = FALSE;
 
-    if (lstrcmpA(szText, "exit") == 0)
+    if (lstrcmpA(szText, "exit") == 0 || lstrcmpA(szText, "exit;") == 0)
         PostMessageW(s_hwndEga, WM_COMMAND, IDCANCEL, 0);
     return true;
 }
