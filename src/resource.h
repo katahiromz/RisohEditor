1130fba4ff3a4e4e2de7326bf6d11bfceade8cf9
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2022-06-06 13:35:26 +0900

    add IDD_TOOLBARRES dialog

--- src/resource.h
+++ src/resource.h
@@ -74,6 +74,7 @@
 #define IDD_DFMSETTINGS                     165
 #define IDD_UILANG                          166
 #define IDD_COPYTOMULTILANG                 167
+#define IDD_TOOLBARRES                      168
 #define IDD_STRINGID                        "StringID"
 
 #define IDI_MAIN                            100
@@ -524,7 +525,7 @@
     #ifndef APSTUDIO_READONLY_SYMBOLS
         #define _APS_NO_MFC                 1
         #define _APS_NEXT_RESOURCE_VALUE    108
-        #define _APS_NEXT_COMMAND_VALUE     273
+        #define _APS_NEXT_COMMAND_VALUE     274
         #define _APS_NEXT_CONTROL_VALUE     1002
         #define _APS_NEXT_SYMED_VALUE       300
     #endif
