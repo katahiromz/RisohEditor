5fc91899166191b25f6c4dec754c18b8958d5e67
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-10-23 08:15:13 +0900

    update resource

--- src/resource.h
+++ src/resource.h
@@ -540,7 +540,7 @@
 #ifdef APSTUDIO_INVOKED
     #ifndef APSTUDIO_READONLY_SYMBOLS
         #define _APS_NO_MFC                 1
-        #define _APS_NEXT_RESOURCE_VALUE    110
+        #define _APS_NEXT_RESOURCE_VALUE    111
         #define _APS_NEXT_COMMAND_VALUE     282
         #define _APS_NEXT_CONTROL_VALUE     1002
         #define _APS_NEXT_SYMED_VALUE       300
