5047a03cb08ba6e948316bed235a3eb15d8065fc
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-10-23 18:55:08 +0900

    Fix double signature problem (#318)

--- do_sign_all.bat
+++ do_sign_all.bat
@@ -1,5 +1,5 @@
 @echo off
-for /R %%f in (*.exe *.dll) do (
+for /R %%f in (build\*.exe build\*.dll *-setup.exe) do (
 	echo "%%~ff" | findstr /I /C:"\CMakeFiles\" /C:"\.git\" /C:"\.vs\" >NUL
 	if errorlevel 1 (
 		call ..\do_sign.bat "%%~ff"
