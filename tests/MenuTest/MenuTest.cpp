0d20bbda24a0cc50bf112d371e7d7865ef5a4984
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-11-30 08:45:56 +0900

    add MenuTest

---
+++ tests/MenuTest/MenuTest.cpp
@@ -0,0 +1,66 @@
+// MenuTest.cpp
+// Test case for TEXTINCLUDE 1 support (Issue #301)
+//////////////////////////////////////////////////////////////////////////////
+
+#include <windows.h>
+#include <windowsx.h>
+#include <commctrl.h>
+#include <tchar.h>
+#include "resource.h"
+
+#ifdef _MSC_VER
+#pragma comment(linker,"/manifestdependency:\"type='win32' \
+	name='Microsoft.Windows.Common-Controls' \
+	version='6.0.0.0' \
+	processorArchitecture='*' \
+	publicKeyToken='6595b64144ccf1df' \
+	language='*'\"")
+#endif
+
+//////////////////////////////////////////////////////////////////////////////
+
+BOOL OnInitDialog(HWND hwnd, HWND hwndFocus, LPARAM lParam)
+{
+	return TRUE;
+}
+
+void OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify)
+{
+	switch (id)
+	{
+	case IDC_BTN_OK:
+	case IDOK:
+		EndDialog(hwnd, IDOK);
+		break;
+	case IDC_BTN_CANCEL:
+	case IDCANCEL:
+		EndDialog(hwnd, IDCANCEL);
+		break;
+	}
+}
+
+INT_PTR CALLBACK
+DialogProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+		HANDLE_MSG(hwnd, WM_INITDIALOG, OnInitDialog);
+		HANDLE_MSG(hwnd, WM_COMMAND, OnCommand);
+	}
+	return 0;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+extern "C" INT WINAPI
+_tWinMain(HINSTANCE   hInstance,
+		  HINSTANCE   hPrevInstance,
+		  LPTSTR      lpCmdLine,
+		  INT         nCmdShow)
+{
+	InitCommonControls();
+	DialogBox(hInstance, MAKEINTRESOURCE(IDD_MAIN), NULL, DialogProc);
+	return 0;
+}
+
+//////////////////////////////////////////////////////////////////////////////
