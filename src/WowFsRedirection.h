8c19af772c401aa1eb48b40e28eaf1383287b35b
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-12-03 12:13:44 +0900

    kill warnings

--- src/WowFsRedirection.h
+++ src/WowFsRedirection.h
@@ -8,20 +8,20 @@ static inline BOOL DisableWow64FsRedirection(PVOID *OldValue)
 {
 	HINSTANCE hKernel32 = GetModuleHandleA("kernel32");
 	FN_Wow64DisableWow64FsRedirection pWow64DisableWow64FsRedirection;
-	pWow64DisableWow64FsRedirection =
-		(FN_Wow64DisableWow64FsRedirection)GetProcAddress(hKernel32, "Wow64DisableWow64FsRedirection");
-	if (pWow64DisableWow64FsRedirection)
-		return (*pWow64DisableWow64FsRedirection)(OldValue);
-	return FALSE;
+	FARPROC fn = GetProcAddress(hKernel32, "Wow64DisableWow64FsRedirection");
+	if (!fn)
+		return FALSE;
+	CopyMemory(&pWow64DisableWow64FsRedirection, &fn, sizeof(fn));
+	return (*pWow64DisableWow64FsRedirection)(OldValue);
 }
 
 static inline BOOL RevertWow64FsRedirection(PVOID OldValue)
 {
 	HINSTANCE hKernel32 = GetModuleHandleA("kernel32");
 	FN_Wow64RevertWow64FsRedirection pWow64RevertWow64FsRedirection;
-	pWow64RevertWow64FsRedirection =
-		(FN_Wow64RevertWow64FsRedirection)GetProcAddress(hKernel32, "Wow64RevertWow64FsRedirection");
-	if (pWow64RevertWow64FsRedirection)
-		return (*pWow64RevertWow64FsRedirection)(OldValue);
-	return FALSE;
+	FARPROC fn = GetProcAddress(hKernel32, "Wow64RevertWow64FsRedirection");
+	if (!fn)
+		return FALSE;
+	CopyMemory(&pWow64RevertWow64FsRedirection, &fn, sizeof(fn));
+	return (*pWow64RevertWow64FsRedirection)(OldValue);
 }
