da79c0c0ad39e4e6bc2fc236350ffd112c2003f7
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2026-05-03 18:19:12 +0900

    fix richedit.h

--- data/lib/gcc/i686-w64-mingw32/10.2.0/include/richedit.h
+++ data/lib/gcc/i686-w64-mingw32/10.2.0/include/richedit.h
@@ -1,24 +1,11 @@
 #pragma once
 
-#define ES_NOOLEDRAGDROP      0x00000008
-#define ES_DISABLENOSCROLL    0x00002000
-#define ES_SUNKEN             0x00004000
-#define ES_SAVESEL            0x00008000
-#define ES_SELFIME            0x00040000
-#define ES_NOIME              0x00080000
-#define ES_VERTICAL           0x00400000
-#define ES_SELECTIONBAR       0x01000000
-#define ES_EX_NOCALLOLEINIT   0x01000000
-
-#ifndef ES_LEFT
-#define ES_LEFT                0x00000000
-#define ES_CENTER              0x00000001
-#define ES_RIGHT               0x00000002
-#define ES_MULTILINE           0x00000004
-#define ES_AUTOVSCROLL         0x00000040
-#define ES_AUTOHSCROLL         0x00000080
-#define ES_NOHIDESEL           0x00000100
-#define ES_READONLY            0x00000800
-#define ES_WANTRETURN          0x00001000
-#define ES_NUMBER              0x00002000
-#endif
+#define ES_NOOLEDRAGDROP 0x8
+#define ES_DISABLENOSCROLL 0x2000
+#define ES_SUNKEN 0x4000
+#define ES_SAVESEL 0x8000
+#define ES_SELFIME 0x40000
+#define ES_NOIME 0x80000
+#define ES_VERTICAL 0x400000
+#define ES_SELECTIONBAR 0x1000000
+#define ES_EX_NOCALLOLEINIT 0x1000000
