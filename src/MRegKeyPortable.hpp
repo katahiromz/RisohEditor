c522847e9eccb8645a7b04d6d42b4c507e476ccd
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2026-08-11 13:52:26 +0900

    MRegKeyPortable::QueryExpandSz

--- src/MRegKeyPortable.hpp
+++ src/MRegKeyPortable.hpp
@@ -175,11 +175,15 @@ LONG MRegKeyPortable::QueryExpandSz(LPCTSTR pszValueName, T_STRING& strValue)
 	{
 		result = RegQueryValueEx(pszValueName, NULL, NULL,
 								 reinterpret_cast<LPBYTE>(psz), &cbData);
-		if (result != ERROR_SUCCESS)
+		if (result == ERROR_SUCCESS)
 		{
 			strValue = psz;
 		}
 		delete[] psz;
+	}
+	else
+	{
+		return ERROR_OUTOFMEMORY;
 	}
 	return result;
 }
