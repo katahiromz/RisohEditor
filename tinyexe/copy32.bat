ceb71615fc4892ab71d70ce38d7ca93a9b598e29
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2026-08-11 20:53:21 +0900

    tinyexe/copy{32,64}.bat

---
+++ tinyexe/copy32.bat
@@ -0,0 +1,2 @@
+copy /Y tiny32.exe ..\src\res\RCData_106.bin
+copy /Y tiny32.dll ..\src\res\RCData_107.bin
---
+++ tinyexe/copy64.bat
@@ -0,0 +1,2 @@
+copy /Y tiny64.exe ..\src\res\RCData_108.bin
+copy /Y tiny64.dll ..\src\res\RCData_109.bin
