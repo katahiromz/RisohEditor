b131c6da87cb2addc2893d046731d58ce14aca21
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2026-08-26 08:14:14 +0900

    fix EntrySet::add_bitmap

--- src/Res.cpp
+++ src/Res.cpp
@@ -1141,7 +1141,8 @@ EntryBase *EntrySet::add_bitmap(const MIdOrString& name, LANGID lang, const MStr
 	if (!stream.LoadFromFile(file.c_str()) || stream.size() <= 4)
 		return nullptr;
 
-	Bitmap_DropFileHeader(stream.data());
+	if (!Bitmap_DropFileHeader(stream.data()))
+		return nullptr;
 
 	HBITMAP hbm = PackedDIB_CreateBitmapFromMemory(&stream[0], stream.size());
 	DeleteObject(hbm);
